1aab9991ab5bc5d9726fff2975ff7355729b14cb
刘磊 <liulei2@aixuedai.com>
2017-09-15 18:26:49 +0800

    22

--- app/src/main/java/com/example/john/activity/CustomProgressActivity.java
+++ app/src/main/java/com/example/john/activity/CustomProgressActivity.java
@@ -17,5 +17,7 @@ public class CustomProgressActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
+
+
     }
 }
